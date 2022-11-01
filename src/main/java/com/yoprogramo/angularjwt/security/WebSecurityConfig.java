1320bf92a0d1c4c54446933d1986f9a5fb9e181c
matias <manevimatias@gmail.com>
2022-11-01 01:35:44 -0300

    correccion de cors en back 5

--- src/main/java/com/yoprogramo/angularjwt/security/WebSecurityConfig.java
+++ src/main/java/com/yoprogramo/angularjwt/security/WebSecurityConfig.java
@@ -63,7 +63,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception{
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/auth/**").permitAll()
+                .antMatchers("https://angularjwtfront.web.app/api/auth/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
